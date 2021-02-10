f770fa79b7693c39fb5fde349daa4e0c366bd9f3
Olof Lindahl <olindahl@gmail.com>
2021-02-10 11:44:18 +0100

    pwa

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { App } from './App';
 import { AppWrapper } from './AppWrapper';
 
 // ReactDOM.render(
@@ -13,52 +12,6 @@ import { AppWrapper } from './AppWrapper';
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
-
-var pStart = {x: 0, y:0};
-var pStop = {x:0, y:0};
-
-function swipeStart(e) {
-    if (typeof e['targetTouches'] !== "undefined"){
-        var touch = e.targetTouches[0];
-        pStart.x = touch.screenX;
-        pStart.y = touch.screenY;
-    } else {
-        pStart.x = e.screenX;
-        pStart.y = e.screenY;
-    }
-}
-
-function swipeEnd(e){
-    if (typeof e['changedTouches'] !== "undefined"){
-        var touch = e.changedTouches[0];
-        pStop.x = touch.screenX;
-        pStop.y = touch.screenY;
-    } else {
-        pStop.x = e.screenX;
-        pStop.y = e.screenY;
-    }
-
-    swipeCheck();
-}
-
-function swipeCheck(){
-    var changeY = pStart.y - pStop.y;
-    var changeX = pStart.x - pStop.x;
-    if (isPullDown(changeY, changeX) ) {
-        alert('Swipe Down!');
-    }
-}
-
-function isPullDown(dY, dX) {
-    // methods of checking slope, length, direction of line created by swipe action 
-    return dY < 0 && (
-        (Math.abs(dX) <= 100 && Math.abs(dY) >= 300)
-        || (Math.abs(dX)/Math.abs(dY) <= 0.3 && dY >= 60)
-    );
-}
-
-document.addEventListener('touchstart', function(e){ swipeStart(e); }, false);
-document.addEventListener('touchend', function(e){ swipeEnd(e); }, false);
 
 ReactDOM.render(<AppWrapper />, document.getElementById('root'))
 
@@ -71,6 +24,4 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
 
